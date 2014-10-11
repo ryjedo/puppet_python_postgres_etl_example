c7cfbd747f5f1d41cf53423b7bb33ae1e879ac46
Ryan J Dobrynski <ryjedo@gmail.com>
2014-10-11 17:52:03 -0600

    adds conversion of list to dict

--- example.py
+++ example.py
@@ -4,12 +4,22 @@ import subprocess
 
 class FacterPoller:
 
-	def harvest_facter_data(self):
+	def get_facter_data_string(self):
 
-		_facter_data = subprocess.check_output(["facter"])
-		return _facter_data
+		_facter_data_string = subprocess.check_output(["facter"])
+		return _facter_data_string
 
 	def get_facter_data_list(self):
 
 		#this works and removes empty lines, but i googled the solution and cannot yet follow the logic.
-		return [line for line in self.harvest_facter_data().split('\n') if line.strip() != '']
+		return [line for line in self.get_facter_data_string().split('\n') if line.strip() != '']
+
+	def get_facter_data_dictionary(self):
+
+		_facter_data_dictionary = {}
+
+		for item in self.get_facter_data_list():
+			_key, _value = item.split(' => ')
+			_facter_data_dictionary[_key] = _value
+
+		return _facter_data_dictionary
--- example_tests.py
+++ example_tests.py
@@ -6,7 +6,7 @@ from example import FacterPoller
 
 class HarvestFacterDataTests(unittest.TestCase):
 
-	__facter_data_string = FacterPoller().harvest_facter_data()
+	__facter_data_string = FacterPoller().get_facter_data_string()
 
 	def test_not_null(self):
 		self.assertNotEqual(self.__facter_data_string, None)
@@ -32,6 +32,27 @@ class SplitFacterDataIntoListTests(unittest.TestCase):
 		self.assertIn("uptime ", self.__facter_data_list[-5])
 		self.assertIn("virtual", self.__facter_data_list[-1])
 
+class ConvertListIntoDictionaryTests(unittest.TestCase):
+
+	__facter_data_dictionary = FacterPoller().get_facter_data_dictionary()
+	__facter_data_list = FacterPoller().get_facter_data_list()
+
+	def test_not_null(self):
+		self.assertNotEqual(self.__facter_data_dictionary, None)
+
+	def test_is_a_list(self):
+		self.assertIsInstance(self.__facter_data_dictionary, dict)
+
+	def test_dict_is_same_size_as_source_list(self):
+		self.assertEqual(len(self.__facter_data_list), len(self.__facter_data_dictionary))
+
+	def test_data_contents_are_sane(self):
+		self.assertNotEqual(self.__facter_data_dictionary['architecture'], "")
+		self.assertNotEqual(self.__facter_data_dictionary['architecture'], None)
+		self.assertNotEqual(self.__facter_data_dictionary['virtual'], "")
+		self.assertNotEqual(self.__facter_data_dictionary['virtual'], None)
+		self.assertNotEqual(self.__facter_data_dictionary['puppetversion'], "")
+		self.assertNotEqual(self.__facter_data_dictionary['puppetversion'], None)
 
 if __name__ == '__main__':
     unittest.main()
